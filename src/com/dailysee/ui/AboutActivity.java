6e60d7997bb18158742851d5d7726bba5530f71c
luohong722 <luohong722@yahoo.cn>
2014-12-06 12:18:48 +0800

    新增 关于、意见反馈和检查更新

---
+++ src/com/dailysee/ui/AboutActivity.java
@@ -0,0 +1,93 @@
+package com.dailysee.ui;
+
+import java.util.List;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.dailysee.R;
+import com.dailysee.ui.base.BaseActivity;
+import com.dailysee.util.Constants;
+import com.dailysee.util.SpUtil;
+import com.dailysee.util.Utils;
+import com.dailysee.widget.ConfirmDialog;
+import com.umeng.fb.FeedbackAgent;
+import com.umeng.update.UmengUpdateAgent;
+
+public class AboutActivity extends BaseActivity implements OnClickListener{
+
+    private LinearLayout llFeedback;
+    private LinearLayout llCheckUpdate;
+	private LinearLayout llCallUs;
+	
+	private TextView tvCheckUpdate;
+    private TextView tvCallUs;
+	private FeedbackAgent mFeedbackAgent;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+	}
+	
+	@Override
+	public void onInit() {
+		setTitle("关于");
+		setUp();
+	}
+
+	@Override
+	public void onFindViews() {
+		llFeedback = (LinearLayout) findViewById(R.id.ll_feed_back);
+		llCheckUpdate = (LinearLayout) findViewById(R.id.ll_check_update);
+		llCallUs = (LinearLayout) findViewById(R.id.ll_call_us);
+		
+		tvCheckUpdate = (TextView) findViewById(R.id.tv_check_update);
+		tvCallUs = (TextView) findViewById(R.id.tv_call_us);
+	}
+
+	@Override
+	public void onInitViewData() {
+		String ver = Utils.getCurrentAppVersionName(this);
+		String version = getResources().getString(R.string.app_version, ver);
+		tvCheckUpdate.setText(version);
+		
+		tvCallUs.setText(Constants.CUSTOMER_SERVICES_PHONE);
+
+        mFeedbackAgent = new FeedbackAgent(this);
+        mFeedbackAgent.sync();
+	}
+
+	@Override
+	public void onBindListener() {
+	    llFeedback.setOnClickListener(this);
+	    llCheckUpdate.setOnClickListener(this);
+	    llCallUs.setOnClickListener(this);
+	}
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_feed_back: // 帮助与反馈
+            	mFeedbackAgent.startFeedbackActivity();
+                break;
+            case R.id.ll_check_update:
+                checkUpdateVersion();
+                break;
+            case R.id.ll_call_us:
+                break;
+        }
+    }
+
+    private void checkUpdateVersion() {
+        // 检查更新
+    	UmengUpdateAgent.forceUpdate(this);
+    }
+    
+}
