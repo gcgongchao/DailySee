347def4b956c67ac357d9dff3f32bcf41f280e9a
luohong722 <luohong722@yahoo.cn>
2014-12-29 17:53:10 +0800

    新增 支付宝快捷支付

--- src/com/dailysee/util/PayUtils.java
+++ src/com/dailysee/util/PayUtils.java
@@ -8,9 +8,9 @@ import java.util.Random;
 public class PayUtils {
 	public static final String PARTNER = "2088711660560521";
 	public static final String SELLER = "ttl66868@163.com";
-	public static final String RSA_PRIVATE = "-----BEGIN RSA PRIVATE KEY-----MIICXQIBAAKBgQChLdGS0RLwINSkO/NgDL7JWNCV63EQ2lbQMg9sLshssNHQVfhkGaTTigSIAK3qAYFv7krIwkxCii/n73EOOut0tUJIXblWMVZWbTDyXT+HIzw2TIk+QFM2bqczOGGiFRnV7PU9v99DOArgjBuz2/HrUoHhYlAQQAMztMokapp6vQIDAQABAoGAWPZeP4gQyOOGGuRQL8q5H49bCfS7Io8w7ZdD9fVYgM2T8UcSY8XXuT7rw1mEpK2SEQLY2jiMOQnm6UC7CRyySodoA6po8AAWZIDfhU+pZpECRLQUGxB88nyYrKzJP0hPSaEdD7eWJIyTGACpdypzfaVAI3z6rHBhkpHB31GficECQQDMEh9MreeQkbVt55Z4k7tSTM328fkGkmpnp/cEZXlbh4MG0cAaO2w8ZoUgD7pN8qLLjzVrxAGtrLhVwvEg3eeRAkEAyjGTSKb6EzWIZTQnl5it5foBBwWzU7Kbict8SHA5Grx0STEi/BoY4T84o8v/wTsTugn/7MswVDql5SoVNPPCbQJBAKRlxQlE39O7Tmmns7SXbsSyUwqpjHAa69VtnrXP+aAe3xNTTmrTjDJsdLqLcQ7ydOuABw017hQ3nJiaXx6XJcECQC6aCIlJOwAsaXhx8eIl5YbI/W2h6bgTxXeX99g2TWMVJnQsKBUU6sEXsjVGcncV57EMVvk7x7LSJaRQ87xVpm0CQQCAlEF8T1UelOn43/iQ5+wFgSx+WQvJFAwS2u4kxrQo/LaslSAR7lV6bzFeMMBGcGs/RvmZiqXpNOrjC1ZeofKi-----END RSA PRIVATE KEY-----";
+	public static final String RSA_PRIVATE = "MIICXQIBAAKBgQChLdGS0RLwINSkO/NgDL7JWNCV63EQ2lbQMg9sLshssNHQVfhkGaTTigSIAK3qAYFv7krIwkxCii/n73EOOut0tUJIXblWMVZWbTDyXT+HIzw2TIk+QFM2bqczOGGiFRnV7PU9v99DOArgjBuz2/HrUoHhYlAQQAMztMokapp6vQIDAQABAoGAWPZeP4gQyOOGGuRQL8q5H49bCfS7Io8w7ZdD9fVYgM2T8UcSY8XXuT7rw1mEpK2SEQLY2jiMOQnm6UC7CRyySodoA6po8AAWZIDfhU+pZpECRLQUGxB88nyYrKzJP0hPSaEdD7eWJIyTGACpdypzfaVAI3z6rHBhkpHB31GficECQQDMEh9MreeQkbVt55Z4k7tSTM328fkGkmpnp/cEZXlbh4MG0cAaO2w8ZoUgD7pN8qLLjzVrxAGtrLhVwvEg3eeRAkEAyjGTSKb6EzWIZTQnl5it5foBBwWzU7Kbict8SHA5Grx0STEi/BoY4T84o8v/wTsTugn/7MswVDql5SoVNPPCbQJBAKRlxQlE39O7Tmmns7SXbsSyUwqpjHAa69VtnrXP+aAe3xNTTmrTjDJsdLqLcQ7ydOuABw017hQ3nJiaXx6XJcECQC6aCIlJOwAsaXhx8eIl5YbI/W2h6bgTxXeX99g2TWMVJnQsKBUU6sEXsjVGcncV57EMVvk7x7LSJaRQ87xVpm0CQQCAlEF8T1UelOn43/iQ5+wFgSx+WQvJFAwS2u4kxrQo/LaslSAR7lV6bzFeMMBGcGs/RvmZiqXpNOrjC1ZeofKi";
 	public static final String RSA_PUBLIC = "";
-	
+
 	/**
 	 * create the order info. 创建订单信息
 	 * 
@@ -35,8 +35,7 @@ public class PayUtils {
 		orderInfo += "&total_fee=" + "\"" + price + "\"";
 
 		// 服务器异步通知页面路径
-		orderInfo += "&notify_url=" + "\"" + "http://notify.msp.hk/notify.htm"
-				+ "\"";
+		orderInfo += "&notify_url=" + "\"" + "http://notify.msp.hk/notify.htm" + "\"";
 
 		// 接口名称， 固定值
 		orderInfo += "&service=\"mobile.securitypay.pay\"";
@@ -68,8 +67,7 @@ public class PayUtils {
 	 * 
 	 */
 	public static String getOutTradeNo() {
-		SimpleDateFormat format = new SimpleDateFormat("MMddHHmmss",
-				Locale.getDefault());
+		SimpleDateFormat format = new SimpleDateFormat("MMddHHmmss", Locale.getDefault());
 		Date date = new Date();
 		String key = format.format(date);
 
