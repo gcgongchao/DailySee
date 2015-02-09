531647c9a4129df8ccb5c71ea3b09bd0dc023fdd
luohong <luohong@fangdd.com>
2015-02-09 15:23:52 +0800

    修改 选择商家房间类型UI

--- src/com/dailysee/adapter/RoomAdapter.java
+++ src/com/dailysee/adapter/RoomAdapter.java
@@ -113,7 +113,7 @@ public class RoomAdapter extends BaseExpandableListAdapter implements OnClickLis
 
 		holder.name.setText(roomType.name);
 		holder.desc.setText(roomType.useDesc);
-		holder.price.setText("原最低消费价:    ¥" + Utils.formatTwoFractionDigits(roomType.amt));		
+		holder.price.setText("原最低消费价    :¥" + Utils.formatTwoFractionDigits(roomType.amt));		
 		String title = "天天最低消费价:¥" + Utils.formatTwoFractionDigits(roomType.ttAmt);
 		SpannableStringBuilder style = new SpannableStringBuilder(title);
 		style.setSpan(new ForegroundColorSpan(context.getResources().getColor(R.color.orange)), 8, title.length(), Spannable.SPAN_EXCLUSIVE_INCLUSIVE); //设置指定位置文字的颜色
